2400c77ee9754e22ddd805365bb0115d1e9dc08b
Makrem hammani <makrem.hamani@esprit.tn>
2019-10-11 21:44:57 +0100

    use pdo

--- config.php
+++ config.php
@@ -6,11 +6,5 @@ $databaseName = 'LyxdNUa4xS';
 $databaseUsername = 'LyxdNUa4xS';
 $databasePassword = '9SlYHcL3Ki';
 
-$conn = mysqli_connect($databaseHost, $databaseUsername, $databasePassword);
-         
-         if(! $conn ) {
-            die('Could not connect: ' . mysqli_error());
-         }
-         echo 'Connected successfully';
-         mysqli_close($conn);
- ?>
+ $conn = new PDO("mysql:host=$databaseHost;dbname=$databaseName",$databaseUsername, $databasePassword);
+?>
