bb1006ff8f5ca4a765bf8c5f3cd9da5873b28555
Makrem hammani <makrem.hamani@esprit.tn>
2019-10-11 21:22:27 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 
 <?php
 
-$databaseHost = '41.226.28.96';
-$databaseName = 'admin_vape_store';
-$databaseUsername = 'makrem';
-$databasePassword = 'makrem';
+$databaseHost = 'https://remotemysql.com/';
+$databaseName = 'LyxdNUa4xS';
+$databaseUsername = 'LyxdNUa4xS';
+$databasePassword = '9SlYHcL3Ki';
  
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 $conn = new PDO("mysql:host=$databaseHost;dbname=$databaseName",$databaseUsername, $databasePassword);
