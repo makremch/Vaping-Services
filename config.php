ae19551340e9f9665759260f7a9c9a86a0703b17
Makrem hammani <makrem.hamani@esprit.tn>
2019-10-11 21:41:22 +0100

    Update config.php

--- config.php
+++ config.php
@@ -5,14 +5,12 @@ $databaseHost = 'remotemysql.com:3306';
 $databaseName = 'LyxdNUa4xS';
 $databaseUsername = 'LyxdNUa4xS';
 $databasePassword = '9SlYHcL3Ki';
- 
 
-try{
- $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
- $conn = new PDO("mysql:host=$databaseHost;dbname=$databaseName",$databaseUsername, $databasePassword);
- echo "connected"
-}
-catch(PDOException $ex){
-   echo (json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
-}
-?>
+$conn = mysqli_connect($databaseHost, $databaseUsername, $databasePassword);
+         
+         if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+         }
+         echo 'Connected successfully';
+         mysqli_close($conn);
+ ?>
