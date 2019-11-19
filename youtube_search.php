dbce19d850c9c4ab7eaf2030929e4f658416e405
Makrem hammani <makrem.hamani@esprit.tn>
2019-11-19 01:26:10 +0100

    adding corse

--- youtube_search.php
+++ youtube_search.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 
 $titre = $_GET['titre']; //Hedhi twali te5ouha parametre
 if(isset($_GET['count'])) {
