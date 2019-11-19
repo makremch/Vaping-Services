f3cfc2c95e769a77edb62dd4835626d7964b4ec9
Makrem hammani <makrem.hamani@esprit.tn>
2019-11-19 02:30:14 +0100

    Update FormulaireAddPost.php

--- FormulaireAddPost.php
+++ FormulaireAddPost.php
@@ -5,9 +5,9 @@ include_once("config.php");
 
 
 if($_GET["action"] == 'ADDPOST'){
-    $sql = "INSERT INTO `articles`(`nomprenom` , `description`, `date`, `time`, `prix`, `idpublicateur` ,`categorie`, `image` ,`image_publicateur`,`ville`,`lat`,`lng` ,`videoid`) VALUES
-     ('".$_GET["nomprenom"]."','".$_GET["description"]."','".$_GET["date"]."','".$_GET["time"]."','".$_GET["prix"]."','".$_GET["idpublicateur"]."','".$_GET["categorie"]."','".$_GET["image"]."','".$_GET["image_publicateur"]."'
-     ,'".$_GET["ville"]."','".$_GET["latitude"]."','".$_GET["langitude"]."','".$_GET["videoid"]."')";
+    $sql = "INSERT INTO `articles`(`nomprenom` , `description`, `prix`, `idpublicateur` ,`categorie`, `image` ,`image_publicateur`,`ville`,`videoid`) VALUES
+     ('".$_GET["nomprenom"]."','".$_GET["description"]."','".$_GET["prix"]."','".$_GET["idpublicateur"]."','".$_GET["categorie"]."','".$_GET["image"]."','".$_GET["image_publicateur"]."'
+     ,'".$_GET["ville"]."','".$_GET["videoid"]."')";
     $result = mysqli_query($mysqli,$sql);
     echo $sql;
     //echo "resultat : ".$result;
